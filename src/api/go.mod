c73b27f2a708c663daa3554f584d2bf6fc017cef
Péter Sturm <sturm.peti@gmail.com>
2024-06-27 18:22:43 +0200

    feat: email about new order

--- src/api/go.mod
+++ src/api/go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	github.com/rs/cors v1.11.0 // indirect
+	github.com/wneessen/go-mail v0.4.1 // indirect
 )
--- src/api/order.go
+++ src/api/order.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"api/internal/configuration"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/wneessen/go-mail"
 )
 
 type Order struct {
@@ -60,6 +64,8 @@ func CreateOrder(response http.ResponseWriter, request *http.Request) {
 	encoder := json.NewEncoder(response)
 	encoder.SetIndent("", "  ")
 	encoder.Encode(order)
+
+	go sendEmail(order)
 }
 
 func InsertOrderToDb(order Order) {
@@ -84,5 +90,29 @@ func InsertOrderToDb(order Order) {
 	err = tx.Commit()
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func sendEmail(order Order) {
+	config := configuration.AppConfig
+
+	email := mail.NewMsg()
+	if err := email.From(config.Email.From); err != nil {
+		log.Printf("Failed to set From: %s", err)
+	}
+	if err := email.To(config.Email.To); err != nil {
+		log.Printf("Failed to set To: %s", err)
+	}
+	email.Subject("New Bakery Order from " + order.Customer)
+	email.SetBodyString(mail.TypeTextPlain, fmt.Sprintf("%s\n%s\n%f\n%s", order.Customer, order.Pastry, order.Quantity, order.PreferedDate.Format("2006-01-02 15:04")))
+
+	client, err := mail.NewClient(config.Email.Smtp.Host, mail.WithPort(config.Email.Smtp.Port), mail.WithSMTPAuth(mail.SMTPAuthLogin),
+		mail.WithUsername(config.Email.Smtp.User), mail.WithPassword(config.Email.Smtp.Pass))
+	if err != nil {
+		log.Printf("failed to create mail client: %s", err)
+	}
+
+	if err := client.DialAndSend(email); err != nil {
+		log.Printf("failed to send mail: %s", err)
 	}
 }
