f45e97038a209e623cd3962a06d04f32f25025d0
Péter Sturm <sturm.peti@gmail.com>
2024-07-09 15:54:00 +0200

    refactor(admin-web): cleanup examples

--- admin-web/src/router/index.js
+++ admin-web/src/router/index.js
@@ -18,14 +18,6 @@ const router = createRouter({
       path: '/orders',
       name: 'orders',
       component: Orders
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
     }
   ]
 })
