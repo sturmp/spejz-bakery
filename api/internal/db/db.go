012d51c411593f282fdb46be188b169f5c162c12
Péter Sturm <sturm.peti@gmail.com>
2024-07-25 11:31:44 +0200

    feat(api): update initial pastries

--- api/internal/db/db.go
+++ api/internal/db/db.go
@@ -17,9 +17,9 @@ var pastries []pastry.Pastry = []pastry.Pastry{
 	{"Foccacia", "Olasz olajos kenyér lángos... Feltét nélkül.", "~440", "db", "~125g"},
 	{"kenyér", "Sima kenyér. Semmi extra.", "600", "db", "750g"},
 	{"English muffin", "Nem, ez nem az édesség. <a href=\"https://www.google.com/search?client=firefox-b-d&q=english+muffin\">Nézz utána!</a>", "150", "db", "65g"},
-	{"Kakaós csiga", "Kakaós és fel van tekerve.", "500", "db", "100g"},
+	{"Kakaós/Fahéjas csiga", "Kakaós(/fahéjas) és fel van tekerve.", "500", "db", "100g"},
 	{"Tortilla", "Mexikói lapos lángos. Kaja origami.", "100", "db", "~20cm"},
-	{"Heti különlegesség", "Kísérleti sütések jól sikerült egyedei.", "TBD", "db", ""},
+	{"Heti különlegesség", "Bagel. Te a kutyára gondolsz, de az beagle. Ez olyan mint a szalagos fánk csak sós.", "350", "db", "~90g"},
 }
 
 var initialOrders []order.Order = []order.Order{}
