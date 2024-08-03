adcccb60717f4009004e43efc31a6cf9a315421d
Péter Sturm <sturm.peti@gmail.com>
2024-08-03 19:56:32 +0200

    feat(admin-web): language switcher

--- admin-web/src/modules/fetch.mjs
+++ admin-web/src/modules/fetch.mjs
@@ -5,7 +5,11 @@ export function fetchFromApi(url, options) {
   if (!options) { options = {} }
   if (!options.headers) { options.headers = {} }
   if (!options.headers["Accept-Language"]) {
-    options.headers["Accept-Language"] = "en"
+    var lang = localStorage.getItem('lang'); 
+    if(lang == null){
+      lang = "hu"
+    }
+    options.headers["Accept-Language"] = lang
   }
   if (!options.headers["AuthToken"]) {
     options.headers["AuthToken"] = import.meta.env.VITE_API_AUTH_TOKEN
--- admin-web/src/views/PastriesView.vue
+++ admin-web/src/views/PastriesView.vue
@@ -3,9 +3,29 @@ import { ref } from 'vue';
 import PastryItem from "../components/PastryItem.vue"
 import PastryEditItem from "../components/PastryEditItem.vue"
 import { fetchFromApi } from '@/modules/fetch.mjs';
+import router from '@/router';
 
 const pastries = ref(null);
-const editedPastry = ref(null)
+const editedPastry = ref(null);
+var language = "";
+
+function initLanguage() {
+    if (localStorage.getItem('lang') == null)
+    {
+        localStorage.setItem('lang', 'hu');
+    }
+
+    language = localStorage.getItem('lang');
+}
+
+function handleLanguageSwitch() {
+    switch(localStorage.getItem('lang')) {
+        case "en": localStorage.setItem('lang', 'hu'); break;
+        case "hu": localStorage.setItem('lang', 'en'); break;
+    }
+
+    router.go(0);
+}
 
 const url =`${import.meta.env.VITE_API_URL}/pastry`;
 async function fetchPastriesAsync() {
@@ -26,10 +46,12 @@ function handleCancel() {
 }
 
 fetchPastriesAsync();
+initLanguage();
 </script>
 
 <template>
-    <div>
+    <div id="container">
+        <button @click="handleLanguageSwitch()">{{ language }}</button>
         <template v-for="(pastry, index) in pastries" v-bind:key=index>
             <PastryItem class="row" v-if="editedPastry == null || pastry.Name != editedPastry.Name"
                 @click="editPastry(pastry)"
@@ -54,6 +76,36 @@ fetchPastriesAsync();
 </template>
 
 <style scoped>
+#container {
+    position: relative;
+}
+
+button {
+    position: absolute;
+    right: -1em;
+    top: -1em;
+    display: flex;
+    z-index: 2;
+
+    font-size: 0.95rem;
+    font-weight: bold;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    height: 2em;
+    width: 2em;
+    background-color: var(--color-background);
+    border: 0.2em dotted var(--border-color);
+    border-radius: 50%;
+
+    cursor: pointer;
+}
+
+button:hover {
+    border: 0.2em dotted var(--border-color-hover);
+}
+
 .row {
     position: relative;
     cursor: pointer;
